997e86b3d535c40728d59e1348efda85518e6947
MD JAHANGIR ALAM <jahangiralam85860@gmail.com>
2022-09-09 22:27:26 +0600

    string and number sum

--- min.js
+++ min.js
@@ -5,3 +5,15 @@ console.log(fullName);
 
 var country = "bangladesh";
 console.log('Hello' + country);
+
+
+
+let num1 ='1'+ 2 + '6';
+// console.log(num1);
+let num2 = num1 - String(1);
+console.log(num2);
+
+const number = [ 120 + 10 + 150 + 20 ];
+const myName = 'md jahangir alam';
+const result = number + ' ' + myName;
+console.log(result);
